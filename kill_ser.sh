d48f21873e04769e72b3804828931ab2dfa38355
zhahantian <2022000917@ruc.edu.cn>
2024-10-01 12:34:41 +0000

    "zht | script"

---
+++ kill_ser.sh
@@ -0,0 +1,2 @@
+ssh -t zhahantian@10.77.110.158 'pkill -f ser_cli'
+ssh -t zhahantian@10.77.110.160 'pkill -f ser_cli'
---
+++ run_ser.sh
@@ -0,0 +1,3 @@
+cd build && ../ser_cli.sh server
+ssh -t zhahantian@10.77.110.158 'cd SepHash/build && ../ser_cli.sh server'
+ssh -t zhahantian@10.77.110.160 'cd SepHash/build && ../ser_cli.sh server'
