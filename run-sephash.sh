b2041618b78e08d019efc1826320ad5f6b90ddf7
Eric Long <i@hack3r.moe>
2025-09-19 00:53:32 +0800

    add pth test script

---
+++ run-sephash.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+scale=10000000
+malloc_size=508468256  # 484M, 10^7
+cache_ratios=(0.01 0.05 0.1 0.2 0.4 0.6 0.8 1.0)
+read_ratios=(100 95 50)
+insert_ratios=(0 5 50)
+my_ip="10.30.2.9"
+
+for _uniform in {0..1}; do
+    for _cache_ratio in ${cache_ratios[@]}; do
+        for _i in {0..2}; do
+            _cache_size=$(echo "$malloc_size * $_cache_ratio / 1" | bc)
+            _read_ratio=${read_ratios[$_i]}
+            _insert_ratio=${insert_ratios[$_i]}
+            if [ $_uniform -eq 1 ]; then
+                _pattern_type=1
+            else
+                _pattern_type=2
+            fi
+            echo running uniform=$_uniform cache_ratio=$_cache_ratio cache_size=$_cache_size read=$_read_ratio insert=$_insert_ratio
+
+            build/ser_cli --server --gid_idx 1 --max_coro 256 --cq_size 64 --mem_size 91268055040 &
+            _memory_pid=$!
+
+            # TODO: warmup
+            PTH_BM_FILENAME="sephash.csv" \
+                PTH_BM_EXTRA_COLS="cache_ratio" \
+                PTH_BM_EXTRA_COL_VALUES="$_cache_ratio" \
+                build/ser_cli \
+                    --server-ip 127.0.0.1 --num_machine 1 --num_cli 1 --num_coro 1 \
+                    --gid_idx 1 \
+                    --max_coro 256 --cq_size 64 \
+                    --machine_id 0 \
+                    --load_num $scale \
+                    --num_op $scale \
+                    --pattern_type $_pattern_type \
+                    --read_frac "$(echo "scale=2; x=$read_ratio/100; if (x<1) print 0; x" | bc)" \
+                    --insert_frac "$(echo "scale=2; x=$insert_ratio/100; if (x<1) print 0; x" | bc)" \
+                    --update_frac  0.0 \
+                    --delete_frac  0.0 \
+                    --read_size     64
+            kill -9 $_memory_pid
+            sleep 3
+        done
+    done
+done
